3dfcc25a120f6e99ff78968aa1f2f137368210d6
nickedizen <nicholasedison00@gmail.com>
2023-10-09 15:15:54 +0700

    Initial Project

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Variabel untuk menyimpan status koneksi internet
+  late bool isConnected;
+
+  // Objek untuk memeriksa koneksi internet
+  final Connectivity _connectivity = Connectivity();
+
+  // Langganan untuk mendengarkan perubahan koneksi
+  StreamSubscription<ConnectivityResult>? _connectivitySubscription;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // Set nilai awal status koneksi sebagai true (terhubung)
+    isConnected = true;
+
+    // Memanggil fungsi pengecekan koneksi internet dan mendengarkan perubahan koneksi setelah selesai
+    _initConnectionStatus().then((_) {
+      _connectivitySubscription =
+          _connectivity.onConnectivityChanged.listen((result) {
+        setState(() {
+          // Perbarui status koneksi jika ada perubahan
+          isConnected = result != ConnectivityResult.none;
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+    // Batalkan langganan saat widget dihancurkan untuk menghindari kebocoran memori
+    _connectivitySubscription?.cancel();
+  }
+
+  // Fungsi untuk memeriksa status koneksi internet saat pertama kali aplikasi dijalankan
+  Future<void> _initConnectionStatus() async {
+    final result = await _connectivity.checkConnectivity();
+    setState(() {
+      // Perbarui status koneksi berdasarkan hasil pengecekan
+      isConnected = result != ConnectivityResult.none;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 1000),
+          child: Image.asset(
+            isConnected ? 'assets/connected.png' : 'assets/disconnected.png',
+            key: ValueKey<bool>(isConnected),
+            width: 200,
+            height: 200,
+          ),
+        ),
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+      ),
+    );
+  }
+}
